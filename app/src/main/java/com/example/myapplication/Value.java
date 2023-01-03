24997e6ef631b2488030ba81ac8582b7639c1a4b
Kuang wei <60306hi603@gmail.com>
2023-01-04 00:54:20 +0800

    add value

--- app/src/main/java/com/example/myapplication/Value.java
+++ app/src/main/java/com/example/myapplication/Value.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import android.app.Application;
 public class Value extends Application{
-    private String FoodName,FoodName1;
+    private String FoodName,FoodName12;
     private int Number,Number1,Number2,Number3,Number4,Number5,Number6,Number7,Number8,Number9,Number10,Number11,
     Number12,Number13,Number14,Number15,Number16,Number17;
 
